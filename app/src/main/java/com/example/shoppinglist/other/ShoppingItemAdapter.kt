8364e376297a09a1e4a78e725b6745f3bf93f9cf
aryn <aryndevelop@gmail.com>
2023-01-29 00:33:11 +0530

    Bug fixes

--- app/src/main/java/com/example/shoppinglist/other/ShoppingItemAdapter.kt
+++ app/src/main/java/com/example/shoppinglist/other/ShoppingItemAdapter.kt
@@ -1,52 +1,49 @@
 package com.example.shoppinglist.other
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.example.shoppinglist.R
 import com.example.shoppinglist.data.db.entities.ShoppingItem
+import com.example.shoppinglist.databinding.ShoppingItemBinding
 import com.example.shoppinglist.ui.shoppinglist.ShoppingViewModel
-import kotlinx.android.synthetic.main.shopping_item.view.*
 
 class ShoppingItemAdapter(
     var items: List<ShoppingItem>,
     private val viewModel: ShoppingViewModel
-    ): RecyclerView.Adapter<ShoppingItemAdapter.ShoppingViewHolder>() {
+): RecyclerView.Adapter<ShoppingItemAdapter.ShoppingViewHolder>() {
 
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShoppingViewHolder {
-            val view = LayoutInflater.from(parent.context).inflate(R.layout.shopping_item, parent, false)
-            return ShoppingViewHolder(view)
-        }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShoppingViewHolder {
+        val binding = ShoppingItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ShoppingViewHolder(binding)
+    }
 
-        override fun onBindViewHolder(holder: ShoppingViewHolder, position: Int) {
-            val curShoppingItem = items[position]
+    override fun onBindViewHolder(holder: ShoppingViewHolder, position: Int) {
+        val curShoppingItem = items[position]
 
-            holder.itemView.tvName.text = curShoppingItem.name
-            holder.itemView.tvAmount.text = "${curShoppingItem.amount}"
+        holder.binding.apply {
+            tvName.text = curShoppingItem.name
+            tvAmount.text = "${curShoppingItem.amount}"
 
-            holder.itemView.ivDelete.setOnClickListener {
+            ivDelete.setOnClickListener {
                 viewModel.delete(curShoppingItem)
             }
 
-            holder.itemView.ivPlus.setOnClickListener {
+            ivPlus.setOnClickListener {
                 curShoppingItem.amount++
                 viewModel.upsert(curShoppingItem)
             }
-
-
-            holder.itemView.ivMinus.setOnClickListener {
+            ivMinus.setOnClickListener {
                 if (curShoppingItem.amount > 0) {
                     curShoppingItem.amount--
                     viewModel.upsert(curShoppingItem)
                 }
             }
         }
+    }
 
-        override fun getItemCount(): Int {
-            return items.size
-        }
+    override fun getItemCount(): Int {
+        return items.size
+    }
 
-        inner class ShoppingViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
-
+    inner class ShoppingViewHolder(val binding: ShoppingItemBinding): RecyclerView.ViewHolder(binding.root)
 }
